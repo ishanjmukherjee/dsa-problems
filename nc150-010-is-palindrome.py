8ac9d67fdca3a979ce99e4de40db16d63111be86
Ishan Mukherjee <ishanjmukherjee@gmail.com>
2024-06-23 07:51:44 -0500

    revisited

--- nc150-010-is-palindrome.py
+++ nc150-010-is-palindrome.py
@@ -67,3 +67,20 @@ class Solution:
             r -= 1
         
         return True  
+
+# New attempt: revisiting problem
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = "".join(s.split()).lower()
+        l, r = 0, len(s) - 1
+        while l < r:
+            while not s[l].isalnum() and l < r:
+                l += 1
+            while not s[r].isalnum() and l < r:
+                r -= 1
+            if s[l] != s[r]:
+                return False
+            l += 1
+            r -= 1
+        return True
