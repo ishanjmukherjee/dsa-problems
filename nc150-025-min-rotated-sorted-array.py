05ce9d8bcf4262e0617dbeefc4874fd42fb409e4
Ishan Mukherjee <ishanjmukherjee@gmail.com>
2024-06-25 02:13:28 -0500

    Create nc150-025-min-rotated-sorted-array.py

---
+++ nc150-025-min-rotated-sorted-array.py
@@ -0,0 +1,15 @@
+# Reimplementing water1111's solution
+# https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/solutions/158940/beat-100-very-simple-python-very-detailed-explanation
+# O(log n) time complexity (as required in problem description), where
+# n : length of nums
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        l, r = 0, len(nums) - 1
+        while l < r:
+            mid = l + (r - l) // 2
+            if nums[mid] > nums[r]:
+                l = mid + 1
+            else:
+                r = mid
+        return nums[l]
