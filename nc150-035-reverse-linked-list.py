0cd24bb4324a862764c383a882f27d4e2f30a498
Ishan Mukherjee <ishanjmukherjee@gmail.com>
2024-07-01 16:24:42 +0530

    revisited

--- nc150-035-reverse-linked-list.py
+++ nc150-035-reverse-linked-list.py
@@ -17,3 +17,24 @@ class Solution:
             prev = curr
             curr = temp
         return prev
+
+# New attempt 1: revisiting
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        curr = head
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        return prev
+
+        
